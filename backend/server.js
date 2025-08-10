eda92b6ec9353a319627c0f48faa5b11e581e9b1
Vardaan Madhisiya <madhisiya.vardan24@gmail.com>
2025-08-10 23:42:58 +0530

    ChatApp

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import path from "path";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+
+const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables.
+const PORT = process.env.PORT || 3000;
+
+// Enable CORS for your frontend URL before any routes or middleware
+app.use(
+	cors({
+		origin: "http://localhost:5173",  // frontend port from vite config
+		credentials: true,                // allow cookies and credentials
+	})
+);
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
